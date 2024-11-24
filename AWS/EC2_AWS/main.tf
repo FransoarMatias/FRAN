b1e4c11f7015c1aace3fcfc6d90b457f93d9ba24
FransoarMatias <fransoarmatias3245@hotmail.com>
2024-11-24 11:27:42 -0300

    ajustes playbook

--- AWS/EC2_AWS/main.tf
+++ AWS/EC2_AWS/main.tf
@@ -24,6 +24,6 @@ resource "aws_instance" "app_server" {
   #            nohup busybox httpd -f -p 8080 &
   #            EOF
   tags = {
-    Name = "SRV_UBUNTU_01"
+    Name = "SRV_UBUNTU_02"
   }
 }
