225468e3b0508bb462bc9e76838a85345fba0724
FransoarMatias <fransoarmatias3245@hotmail.com>
2024-11-26 21:00:26 -0300

    ajustes dev

--- AWS/EC2_AWS_P2/env/dev/main.tf
+++ AWS/EC2_AWS_P2/env/dev/main.tf
@@ -1,7 +1,7 @@
 module "aws-dev" {
   source = "../../infra"
   instancia = "t2.micro"
-  regiao_aws = "sa-wast-1"
+  regiao_aws = "sa-east-1"
   chave = "iac-dev"
   aim_instance = "ami-06dec7e27b4abea"
 }
