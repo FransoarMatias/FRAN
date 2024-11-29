ae8a9cc1fb7631feec61f5561333c6349c5a9fc9
FransoarMatias <fransoarmatias3245@hotmail.com>
2024-11-28 22:40:10 -0300

    commit

--- AWS/EC2_AWS_P2/env/prod/main.tf
+++ AWS/EC2_AWS_P2/env/prod/main.tf
@@ -1,8 +1,12 @@
-module "aws-dev" {
+module "aws-prod" {
   source = "../../infra"
   instancia = "t2.micro"
   regiao_aws = "sa-east-1"
   chave = "iac-prod"
   aim_instance = "ami-06dec7e27b4abea7b"
   name_db = "SRV_PROD_01"
+}
+
+output "IP_EC2_PROD" {
+  value = module.aws-prod.IP_Publico
 }
