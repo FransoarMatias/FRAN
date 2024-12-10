f2870b7adec9f53871249ea57a399fc8670cf5a9
FransoarMatias <fransoarmatias3245@hotmail.com>
2024-12-10 20:44:29 -0300

    commit

--- myself/AWS/SRV_ORACLE/env/main.tf
+++ myself/AWS/SRV_ORACLE/env/main.tf
@@ -2,7 +2,7 @@ module "aws-dev" {
   source = "../infra"
   instancia = "t2.micro" # Nivel Gratuito
   regiao_aws = "sa-east-1" # Sao Paulo
-  chave = "iac-dev"
+  chave = "oracledb"
   aim_instance = "ami-06dec7e27b4abea7b" # Red Hat
   name_db = "SRV_ORACLE"
 }
