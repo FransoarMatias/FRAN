b87bce815c00a592d99aa143755485541afba88f
FransoarMatias <fransoarmatias3245@hotmail.com>
2024-12-11 20:42:18 -0300

    commit

--- myself/AWS/SRV_ORACLE/env/main.tf
+++ myself/AWS/SRV_ORACLE/env/main.tf
@@ -1,12 +1,12 @@
-module "aws-dev" {
+module "aws-oracle" {
   source = "../infra"
   instancia = "t2.micro" # Nivel Gratuito
-  regiao_aws = "sa-east-1" # Sao Paulo
+  regiao_aws = "us-west-2" # Oregon
   chave = "oracledb"
-  aim_instance = "ami-06dec7e27b4abea7b" # Red Hat
+  aim_instance = "ami-0423fca164888b941" # Red Hat
   name_db = "SRV_ORACLE"
 }
 
 output "IP_ORACLE" {
-  value = module.aws-dev.IP_Publico
+  value = module.aws-oracle.IP_Publico
 }
