7cd46fd61a6070e0f0f45a806c4ac496ff5e4577
FransoarMatias <fransoarmatias3245@hotmail.com>
2025-01-21 20:29:38 -0300

    commit

--- myself/AWS/SRV_ORACLE/env/main.tf
+++ myself/AWS/SRV_ORACLE/env/main.tf
@@ -3,7 +3,7 @@ module "aws-oracle" {
   instancia = "t2.micro" # Nivel Gratuito
   regiao_aws = "us-west-2" # Oregon
   chave = "oracledb"
-  aim_instance = "ami-0423fca164888b941" # Red Hat
+  aim_instance = "ami-00c257e12d6828491" # Ubuntu
   name_db = "SRV_ORACLE"
 }
 
